6c57dcfdc37a1dba15b19fa478b92f13dcac03af
Alejandro López <aloro2392@gmail.com>
2019-02-22 21:16:26 +0100

    Logger
    Log con trazas de debug, info y error.

--- HashCode 2019/src/com/nevergoes/App.java
+++ HashCode 2019/src/com/nevergoes/App.java
@@ -1,8 +1,10 @@
 package com.nevergoes;
 
+import com.nevergoes.log.MyLog;
+
 public class App {
 	
 	public static void main(String[] args) {
-		
+		System.setProperty("debug", "debug");
 	}
 }
---
+++ HashCode 2019/src/com/nevergoes/log/MyLog.java
@@ -0,0 +1,25 @@
+package com.nevergoes.log;
+
+public class MyLog {
+	
+	private static String property = System.getProperty("debug");
+	
+	public static void debug(String msg) {
+		if(property.equals("debug")) {
+			System.out.println("[DEBUG] " + msg);
+		}
+	}
+	
+	public static void info(String msg) {
+		if(property.equals("debug") || property.equals("info")) {
+			System.out.println("[INFO] " + msg);
+		}
+	}
+	
+	public static void error(String msg) {
+		if(property.equals("error") || property.equals("info") || property.equals("debug")) {
+			System.out.println("[ERROR] " + msg);
+		}
+	}
+
+}
