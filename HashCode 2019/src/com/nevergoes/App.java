096c0be5fda345231800b9004fd10516787f55bb
jmuelape <jaimuepe@gmail.com>
2019-02-22 20:48:00 +0100

    IO changes

--- HashCode 2019/src/com/nevergoes/App.java
+++ HashCode 2019/src/com/nevergoes/App.java
@@ -1,9 +1,8 @@
 package com.nevergoes;
 
-import com.nevergoes.io.MyFiles;
-
 public class App {
+	
 	public static void main(String[] args) {
-		MyFiles.read("a");
+		
 	}
 }
--- HashCode 2019/src/com/nevergoes/io/MyFiles.java
+++ HashCode 2019/src/com/nevergoes/io/MyFiles.java
@@ -1,5 +1,7 @@
 package com.nevergoes.io;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Paths;
@@ -8,21 +10,20 @@ import java.util.List;
 public class MyFiles {
 
 	public static List<String> read(String fileName) {
-		
+
+		final URL fileResource = MyFiles.class.getClassLoader().getResource(fileName);
+
+		if (fileResource == null) {
+			System.err.print("File " + "\"" + fileName + "\"" + " not found!");
+			return null;
+		}
+
 		try {
-			final URL fileResource = MyFiles.class.getClassLoader().getResource(fileName);
-			
-			if (fileResource == null) {
-				System.err.print("File " + "\"" + fileName + "\"" + " not found!");
-				return null;
-			}
-			
 			return Files.readAllLines(Paths.get(fileResource.toURI()));
-			
-		} catch (Exception e) {
+		} catch (IOException | URISyntaxException e) {
 			e.printStackTrace();
 		}
-		
+
 		return null;
 	}
 }
