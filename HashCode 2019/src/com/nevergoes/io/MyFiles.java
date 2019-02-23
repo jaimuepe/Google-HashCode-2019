5566d960bd1eb33d4840c9a3eeb899df20baa0b8
Alejandro López <aloro2392@gmail.com>
2019-02-23 12:19:54 +0100

    Method to write in file

--- HashCode 2019/src/com/nevergoes/io/MyFiles.java
+++ HashCode 2019/src/com/nevergoes/io/MyFiles.java
@@ -4,12 +4,16 @@ import java.io.IOException;
 import java.net.URISyntaxException;
 import java.net.URL;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.List;
+
+import com.nevergoes.log.MyLog;
 
 public class MyFiles {
 
-	public static List<String> read(String fileName) {
+	public static List<String> readFile(String fileName) {
 
 		final URL fileResource = MyFiles.class.getClassLoader().getResource(fileName);
 
@@ -25,5 +29,19 @@ public class MyFiles {
 		}
 
 		return null;
+	}
+
+	public static void writeFile(String fileName, String text) {
+		if (fileName == null) {
+			MyLog.error("You must provide a fileName");
+		} else if (text == null) {
+			MyLog.error("You must provide a text to write the new file.");
+		}
+
+		try {
+			Files.write(Paths.get("resources/" + fileName), text.getBytes(), StandardOpenOption.CREATE);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
--- HashCode 2019/src/com/nevergoes/io/MyFilesTest.java
+++ HashCode 2019/src/com/nevergoes/io/MyFilesTest.java
@@ -1,5 +1,7 @@
 package com.nevergoes.io;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 
 import com.nevergoes.assertions.MyAsserts;
@@ -10,7 +12,13 @@ public class MyFilesTest {
 
 		System.out.println("Running test: " + MyFilesTest.class.getSimpleName());
 
-		List<String> lines = MyFiles.read("testFile.txt");
+		List<String> lines = MyFiles.readFile("testFile.txt");
+		// TODO hacer test writeFile
 		MyAsserts.isTrue(lines.size() == 3, "Fileread test failed!");
+		
+		// Test writeFile
+		MyFiles.writeFile("test.txt", "Testing");
+		Path path = Paths.get("resources/test.txt");
+		MyAsserts.isTrue(path.toFile().isFile(), "File writing test failed!");
 	}
 }
